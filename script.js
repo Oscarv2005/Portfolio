74449ff9a484be32f9bafc247344aa95c091b538
Oscarv2005 <oscarvishnoi0@gmail.com>
2026-03-22 23:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
+// MENU TOGGLE
 const toggle = document.querySelector(".menu-toggle")
 const nav = document.querySelector(".nav-links")
 
-if(toggle){
-toggle.addEventListener("click",()=>{
+if (toggle) {
+toggle.addEventListener("click", () => {
 nav.classList.toggle("active")
 })
 }
 
-const observer = new IntersectionObserver((entries)=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
+// SCROLL REVEAL
+const observer = new IntersectionObserver((entries) => {
+entries.forEach(entry => {
+if (entry.isIntersecting) {
 entry.target.classList.add("reveal")
 }
 })
-},{threshold:0.15})
+}, { threshold: 0.15 })
 
 document.querySelectorAll(
 ".project-card,.certificate-card,.skill-item,.edu-card"
-).forEach(el=>observer.observe(el))
+).forEach(el => observer.observe(el))
 
 // EMAILJS START
-(function(){
-emailjs.init("26uNHLamLEkAAw8XG");
+(function () {
+emailjs.init("26uNHLamLEkAAw8XG"); // Your Public Key
 })();
 
 const form = document.getElementById("contactForm")
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", function (e) {
 e.preventDefault()
 
 const btn = form.querySelector("button")
-btn.innerText="Sending..."
+btn.innerText = "Sending..."
 
-emailjs.send("service_bcf3ya5","template_je6ty87")
-.then(()=>{
+emailjs.sendForm(
+"service_bcf3ya5",     // Service ID
+"template_je6ty87",    // Template ID
+this                   // Form
+)
+.then(function () {
 alert("Message sent successfully!")
-btn.innerText="Send Message"
+btn.innerText = "Send Message"
 form.reset()
 })
-.catch((error)=>{
+.catch(function (error) {
 alert("Failed to send message")
 console.log(error)
+btn.innerText = "Send Message"
 })
 })
